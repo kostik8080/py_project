9c08765e886137198ca84f0bc25cf39b037b1143
Konstantin Onishchenko <kostik80_80@mail.ru>
2023-10-11 19:06:12 +0300

    добавление второй функции

--- func.py
+++ func.py
@@ -1,3 +1,15 @@
 def numbers(a, b):
     total = a + b
     return total
+
+
+number_addition = numbers(12, 35)
+print(number_addition)
+
+def numbers_2(a, b):
+    total = a - b
+    return total
+
+
+number_subtraction = numbers_2(87, 43)
+print(number_subtraction)
